13a91cb80506ccc762e0ac3adbb0f9b8ad2f8e15
Jannik0308 <jnkarger01@gmail.com>
2022-05-01 21:37:33 +0200

    Fixed builders regex

--- src/entity/Block.ts
+++ src/entity/Block.ts
@@ -54,7 +54,7 @@ export class Block extends BaseEntity {
   details: boolean;
 
   @Column("text", { nullable: true })
-  @Matches(/^([a-zA-Z0-9_]{3,16}[,]?){1,}$/, {
+  @Matches(/^$|^(([a-zA-Z0-9_]{3,16}[,]?){1,})|Building Session$/, {
     message: "Invalid Minecraft-Name found or not separated with a comma",
   })
   @IsOptional()
@@ -64,7 +64,7 @@ export class Block extends BaseEntity {
   @IsOptional()
   completionDate: Date;
 
-  @Column("text", /*{ default: "[]" }*/)
+  @Column("text" /*{ default: "[]" }*/)
   location: string;
 
   async toJson({
