9e2a4712a10571ac7f220976ae20119967e95465
Jannik0308 <hightower3801@gmail.com>
2022-11-21 22:18:11 +0100

    Start script for local database

--- src/data-sources.ts
+++ src/data-sources.ts
@@ -14,3 +14,17 @@ export const AppDataSource = new DataSource({
   migrations: [],
   subscribers: [],
 });
+
+export const LocalAppDataSource = new DataSource({
+  type: "mysql",
+  host: "127.0.0.1",
+  port: 3306,
+  username: "mfprogress",
+  password: "M^cB9&u*2dw2",
+  database: "mfprogress",
+  synchronize: true,
+  logging: false,
+  entities: ["src/entity/**/*.ts"],
+  migrations: [],
+  subscribers: [],
+});
