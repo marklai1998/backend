f82769fd59bdc4a58afb92852855f65264dd14db
Jannik0308 <hightower3801@gmail.com>
2023-04-15 19:43:07 +0200

    read db from ormconfig

--- src/data-sources.ts
+++ src/data-sources.ts
@@ -1,13 +1,15 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
 
+const ormconfig = require("../ormconfig.json");
+
 export const AppDataSource = new DataSource({
   type: "mysql",
-  host: "144.217.77.29",
-  port: 3306,
-  username: "mfprogress",
-  password: "M^cB9&u*2dw2",
-  database: "mfprogress",
+  host: ormconfig.host,
+  port: ormconfig.port,
+  username: ormconfig.username,
+  password: ormconfig.password,
+  database: ormconfig.database,
   synchronize: true,
   logging: false,
   entities: ["src/entity/**/*.ts"],
@@ -18,10 +20,10 @@ export const AppDataSource = new DataSource({
 export const LocalAppDataSource = new DataSource({
   type: "mysql",
   host: "127.0.0.1",
-  port: 3306,
-  username: "mfprogress",
-  password: "M^cB9&u*2dw2",
-  database: "mfprogress",
+  port: ormconfig.port,
+  username: ormconfig.username,
+  password: ormconfig.password,
+  database: ormconfig.database,
   synchronize: true,
   logging: false,
   entities: ["src/entity/**/*.ts"],
