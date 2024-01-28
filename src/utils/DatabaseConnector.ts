cd38be1b2b2992c491174483de0e7779b4c68a1c
Jannik0308 <hightower3801@gmail.com>
2024-01-28 15:17:59 +0100

    Auto reconnect on db disconnect

--- src/utils/DatabaseConnector.ts
+++ src/utils/DatabaseConnector.ts
@@ -1,3 +1,4 @@
+import { Colors, sendWebhook } from "./DiscordMessageSender";
 import Logger from "./Logger";
 
 const mysql = require("mysql");
@@ -27,6 +28,46 @@ function connectToDatabase(db: string) {
     database: db,
   });
   DATABASES[db].connect();
+
+  DATABASES[db].on("error", (err) => {
+    this.core.getLogger().error(`MySQL error: ${err}`);
+    sendWebhook("error_log", {
+      content: "",
+      embeds: [
+        {
+          title: "Database Error Occurred",
+          description: "",
+          color: Colors.Error,
+          timestamp: new Date().toISOString(),
+          footer: {
+            text: "MineFact Network",
+            icon_url:
+              "https://cdn.discordapp.com/avatars/422633274918174721/7e875a4ccb7e52097b571af1925b2dc1.png",
+          },
+          fields: [
+            {
+              name: "Name",
+              value: err.code,
+              inline: true,
+            },
+            {
+              name: "Message",
+              value: "Please verify if the automatic reconnect was successful!",
+              inline: true,
+            },
+          ],
+        },
+      ],
+    });
+
+    if (err.code === "ECONNRESET") {
+      // Reconnect if the connection is lost
+      DATABASES[db].connect();
+    } else {
+      throw err;
+    }
+  });
+
   Logger.debug(`Connected to ${db} Database`);
 }
 
